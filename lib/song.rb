025d28c67ad79721843ee03bd6b82b96acee0da9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-11 06:36:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,8 +51,8 @@ class Song
     song_array = filename.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3") #removing record separater
     song = self.new
-    song.name = song_array[0]
-    song.artist_name = song_array[1]
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
   end
 
 end
