782cea8ceed901d343072ba0f320d41c0a790315
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-11 06:35:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,6 +50,9 @@ class Song
   def new_from_filename(filename)
     song_array = filename.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3") #removing record separater 
+    song = self.new
+    song.name = song_array[0]
+    song.artist_name = song_array[1]
   end
 
 end
