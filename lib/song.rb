253bc2689aafa99c754dd46de2f5f88ca280cb8c
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-11 06:01:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,22 @@ class Song
     self.class.all << self
   end
 
+  def self.create #this builds the class constructor
+    song = self.new
+    self.all << song #this initializes a song and saves it to the @all class variable
+    song #returns song instance that has been initialized and saved
+  end
+
+  def self.new_by_name(title)
+    song = self.new #instantiates
+    song.name = title #takes in the string name of a song and returns a song instance with name set as name property
+    song
+  end
+
+  def create_by_name(title)
+    song = self.create
+    song.name = title
+    song
+  end
+
 end
